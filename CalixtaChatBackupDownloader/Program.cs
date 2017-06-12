9c394b24c9f1c878fd2486dc3256ee9e65150d44
Alexis Cervantes <pacharrin@gmail.com>
2017-06-12 13:57:41 -0500

    Agregar archivos de proyecto.

---
+++ CalixtaChatBackupDownloader/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalixtaChatBackupDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string client = string.Empty;
+            string token = string.Empty;
+
+
+
+            if (args == null)
+            {
+                Console.WriteLine("Not enough params");
+                Console.WriteLine("usage CalixtaChatBackupDownloader client token folder"); 
+            } else
+            {
+
+            }
+            Console.WriteLine("Download backups from calixta chat");
+        }
+    }
+}
